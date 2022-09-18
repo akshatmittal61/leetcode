04c46f96409fb8cc7598026cd0c31b76122c5665
Akshat Mittal <akshatmittal2506@gmail.com>
2022-09-18 10:38:30 +0530

    Time: 3 ms (60.82%), Space: 6 MB (27.94%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0)return false;
+        if(n!=1 && (n&1))return false;
+        while(n%2==0)
+        {
+            n/=2;
+            if(n!=1 && (n&1))return false;
+        }
+        return true;
+    }
+};
