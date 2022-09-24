cbd3ac4e627cc89861ce6e476be24c98916b42c3
Akshat Mittal <akshatmittal2506@gmail.com>
2022-09-24 20:11:51 +0530

    Time: 7 ms (92.52%), Space: 9.3 MB (54.06%) - LeetHub

--- 883-projection-area-of-3d-shapes/883-projection-area-of-3d-shapes.cpp
+++ 883-projection-area-of-3d-shapes/883-projection-area-of-3d-shapes.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     int projectionArea(vector<vector<int>>& v) {
-        int ans=0;
-        for(auto r:v)
-        {
-            for(auto i:r)
-                ans+=(i>0);
-        }
-        int n=v.size();
+        int ans=0,n=v.size();
         for(int i=0;i<n;++i)
         {
-            int l=v[i][0];
+            int p=v[i][0],q=v[0][i];
             for(int j=0;j<n;++j)
-                l=max(l,v[i][j]);
-            ans+=l;
-        }
-        for(int i=0;i<n;++i)
-        {
-            int l=v[0][i];
-            for(int j=0;j<n;++j)
-                l=max(l,v[j][i]);
-            ans+=l;
+            {
+                p=max(p,v[i][j]);
+                q=max(q,v[j][i]);
+                ans+=(v[i][j]>0);
+            }
+            ans+=(p+q);
         }
         return ans;
     }
