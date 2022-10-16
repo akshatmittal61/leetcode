c6f0937e6efef0b4ef2dac0fbe6edea6f06dc85c
Akshat Mittal <akshatmittal2506@gmail.com>
2022-10-16 06:41:23 +0530

    Time: 21 ms (97.45%), Space: 7.2 MB (93.10%) - LeetHub

---
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+    public:
+        int minDifficulty(vector<int> &jobDifficulty, int d)
+        {
+            int n = jobDifficulty.size();
+
+            if (n < d) return -1;
+
+            int dp[d][n];
+
+            dp[0][0] = jobDifficulty[0];
+            for (int i = 1; i < n; i++)
+                dp[0][i] = max(dp[0][i - 1], jobDifficulty[i]);
+
+            for (int i = 1; i < d; i++)
+            {
+                for (int j = i; j < n; j++)
+                {
+                    int mxLocal = jobDifficulty[j];
+                    int mnVal = INT_MAX;
+                    for (int k = j; k >= i; k--)
+                    {
+                        mxLocal = max(mxLocal, jobDifficulty[k]);
+                        mnVal = min(mnVal, dp[i - 1][k - 1] + mxLocal);
+                    }
+                    dp[i][j] = mnVal;
+                }
+            }
+            return dp[d - 1][n - 1];
+        }
+};
