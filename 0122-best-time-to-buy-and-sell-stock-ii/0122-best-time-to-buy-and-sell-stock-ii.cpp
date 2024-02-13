ee6d2eef04eb66a335bf8bba0f1c0292ffe89e29
Akshat Mittal <akshatmittal2506@gmail.com>
2024-02-13 23:19:31 +0530

    Time: 3 ms (89.85%), Space: 15.3 MB (55.55%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& nums) {
+        int n = nums.size(), ans = 0;
+        for(int i = 1; i < n; ++i) {
+            if( nums[i] > nums[i - 1] )
+                ans += nums[i] - nums[i - 1];
+        }
+        return ans;
+    }
+};
