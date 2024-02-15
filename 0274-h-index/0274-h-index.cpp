3d52fd3868008f515641ff82f84a54bf07ef645c
Akshat Mittal <akshatmittal2506@gmail.com>
2024-02-15 12:58:21 +0530

    Time: 0 ms (100%), Space: 10.6 MB (40.62%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -7,14 +7,14 @@ public:
                 return 1;
             return 0;
         }
-        if(nums[n - 1] == 0)
-            return 0;
         sort(nums.begin(), nums.end());
         for(int i = n - 1; i >= 0; --i) {
             if( nums[i] >= n - i )
                 continue;
-            else
+            else {
+                if( i == n - 1 ) return 0;
                 return min(nums[i + 1], n - i - 1);
+            }
         }
         return min(nums[0], n);
     }
