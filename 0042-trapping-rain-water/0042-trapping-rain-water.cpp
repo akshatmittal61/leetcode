fbc6302272627937b8acc448880ed3045aff1031
Akshat Mittal <akshatmittal2506@gmail.com>
2024-02-17 23:09:46 +0530

    Time: 7 ms (91.52%), Space: 22.4 MB (22.05%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,16 +2,24 @@ class Solution {
 public:
     int trap(vector<int>& v) {
         int n = v.size(), ans = 0;
-        vector<int> lm(n, 0), rm(n, 0);
-        lm[0] = v[0], rm[n - 1] = v[n - 1];
-        for(int i = 1, j = n - 2; i < n && j >= 0; ++i, --j) {
-            lm[i] = max(lm[i - 1], v[i]);
-            rm[j] = max(rm[j + 1], v[j]);
-        }
-        for(int i = 0; i < n; ++i) {
-            int lr = min(lm[i], rm[i]);
-            // add to ans, if any amount of water can be added
-            ans += max(0, lr - v[i]);
+        int l = 0, r = n - 1;
+        int lm = v[0], rm = v[n - 1];
+        while(l < r) {
+            if(v[l] < v[r]) {
+                if(v[l] < lm) {
+                    ans += min(lm, v[r]) - v[l];
+                } else {
+                    lm = v[l];
+                }
+                ++l;
+            } else {
+                if(v[r] < rm) {
+                    ans += min(rm, v[l]) - v[r];
+                } else {
+                    rm = v[r];
+                }
+                --r;
+            }
         }
         return ans;
     }
