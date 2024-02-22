bc5ef642086dc40115122aacc941f486f7edfcb1
Akshat Mittal <akshatmittal2506@gmail.com>
2024-02-23 02:08:29 +0530

    Time: 0 ms (100%), Space: 7.5 MB (47.32%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -11,18 +11,11 @@ public:
                     v[i][j] = 0;
             }
         }
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < h; ++j) {
-                cout<<v[i][j]<<' ';
-            }
-            cout<<endl;
-        }
         for (int i = 0; i < h; ++i) {
             int c = 0;
             int j = 0, k = i;
             while( j < n && k < h && v[j][k] == 1 )
                 ++c, ++j, ++k;
-            cout<<i<<' '<<c<<endl;
             if (c == n)
                 return i;
         }
