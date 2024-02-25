01b2791450fb7be83beacbba6e2d2cc94889030b
Akshat Mittal <akshatmittal2506@gmail.com>
2024-02-25 23:32:53 +0530

    Time: 52 ms (95.54%), Space: 27.1 MB (57.73%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,42 +1,29 @@
 class Solution {
 public:
-    int _(vector<int> v, vector<int> t) {
-        int a = v[t[0]];
-        int b = v[t[1]];
-        int c = v[t[2]];
-        if (a + b + c == 0) return 0;
-        else if (a + b + c > 0) return 1;
-        else return -1;
-    }
     vector<vector<int>> threeSum(vector<int>& v) {
         sort(v.begin(), v.end());
         vector<vector<int>> ans;
         int n = v.size();
-        int i = 0;
-        while ( i < n - 2 ) {
+        for (int i = 0; i < n - 2; ++i ) {
             int low = i + 1, high = n - 1;
             while ( low < high ) {
-                int k = _(v, {i, low, high});
+                int l = v[i] + v[low] + v[high];
+                int k = 0;
+                if ( l > 0 ) k = 1;
+                else if ( l < 0 ) k = -1;
                 if ( k == 0 ) {
                     ans.push_back({v[i], v[low], v[high]});
-                    while( low < high && v[low] == v[low + 1] )
-                        ++low;
-                    while( high > low && v[high] == v[high - 1] )
-                        --high;
-                    --high, ++low;
+                    while( high > low && v[high] == v[high - 1] ) --high;
+                    while( low < high && v[low] == v[low + 1] ) ++low;
+                    ++low, --high;
                 } else if ( k == 1 ) {
-                    do {
-                        --high;
-                    } while( high > low && v[high] == v[high - 1] );
+                    --high;
                 } else {
-                    do {
-                        ++low;
-                    } while( low < high && v[low] == v[low + 1] );
+                    ++low;
                 }
             }
-            do {
+            while( i < n - 1 && v[i] == v[i + 1] )
                 ++i;
-            } while( v[i] == v[i - 1] );
         }
         return ans;
     }
