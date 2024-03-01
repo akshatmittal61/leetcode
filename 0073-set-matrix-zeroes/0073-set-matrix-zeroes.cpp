e2148df07b4afbe41352b250b1814de1df481dd5
Akshat Mittal <akshatmittal2506@gmail.com>
2024-03-02 00:54:49 +0530

    Time: 11 ms (53.1%), Space: 16.5 MB (7.79%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,55 +1,22 @@
-class Solution
-{
-    public:
-        void setZeroes(vector<vector < int>> &matrix)
-        {
-            int m = matrix.size(), n = matrix[0].size();
-            bool r0 = false, c0 = false;
-            for (int i = 0; i < m; ++i)
-            {
-                if (matrix[i][0] == 0)
-                {
-                    c0 = true;
-                    break;
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        set<int>r, c;
+        int m = matrix.size(), n = matrix[0].size();
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j) {
+                if(matrix[i][j] == 0) {
+                    r.insert(i);
+                    c.insert(j);
                 }
-            }
-            for (int i = 0; i < n; ++i)
-            {
-                if (matrix[0][i] == 0)
-                {
-                    r0 = true;
-                    break;
-                }
-            }
-            for (int i = 0; i < m; ++i)
-            {
-                for (int j = 0; j < n; ++j)
-                {
-                    if (matrix[i][j] == 0)
-                        matrix[0][j] = 0, matrix[i][0] = 0;
-                }
-            }
-            for (int i = 1; i < m; ++i)
-            {
-                if (matrix[i][0] == 0)
-                    for (int j = 1; j < n; ++j)
-                        matrix[i][j] = 0;
-            }
-            for (int j = 1; j < n; ++j)
-            {
-                if (matrix[0][j] == 0)
-                    for (int i = 1; i < m; ++i)
-                        matrix[i][j] = 0;
-            }
-            if (c0)
-            {
-                for (int j = 0; j < m; ++j)
-                    matrix[j][0] = 0;
-            }
-            if (r0)
-            {
-                for (int i = 0; i < n; ++i)
-                    matrix[0][i] = 0;
             }
         }
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j) {
+                if(r.find(i) != r.end() || c.find(j) != c.end()) {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
 };
