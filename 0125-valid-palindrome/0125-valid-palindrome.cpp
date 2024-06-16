23b114fffd70048cba57d81bfdadcead6c6bc2c5
Akshat Mittal <akshatmittal2506@gmail.com>
2024-06-16 12:29:21 +0530

    Time: 4 ms (79.73%), Space: 9.2 MB (19.27%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,36 +1,15 @@
 class Solution {
-    char getChar(char c) {
-        if ( c >= 'a' && c <= 'z' ) {
-            return c;
-        } else if ( c >= 'A' && c <= 'Z' ) {
-            return (c - 'A' + 'a');
-        } else if ( c >= '0' && c <= '9' ) {
-            return c;
-        }
-        return '-';
-    }
 public:
     bool isPalindrome(string s) {
-        int n = s.length();
-        for (int i = 0, j = n - 1; i < j;) {
-            char a = getChar(s[i]);
-            char b = getChar(s[j]);
-            cout<<a<<':'<<b<<endl;
-            if ( a == '-' && b == '-' ) {
-                ++i, --j;
-                continue;
-            } else if ( a == '-' ) {
-                ++i;
-                continue;
-            } else if ( b == '-' ) {
-                --j;
-                continue;
-            } else {
-                ++i, --j;
-                if ( a != b ) {
-                    return false;
-                }
-            }
+        string str = "";
+        for(auto c: s) {
+            if (c >= 'a' && c <= 'z') str += c;
+            else if (c >= 'A' && c <= 'Z') str += c - 'A' + 'a';
+            else if (c >= '0' && c <= '9') str += c;
+        }
+        int n = str.length();
+        for (int i = 0, j = n - 1; i < j; ++i, --j) {
+            if (str[i] != str[j]) return false;
         }
         return true;
     }
