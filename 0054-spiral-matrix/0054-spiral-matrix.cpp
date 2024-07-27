ee20d214cba719b99595eb3398ca519d4f1f8a5e
Akshat Mittal <akshatmittal2506@gmail.com>
2024-07-27 16:34:32 +0530

    Time: 3 ms (37.97%), Space: 8.5 MB (12.8%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int top = 0, right = n - 1, bottom = m - 1, left = 0;
-        char c = 'e';
+        int r = matrix.size();
+        int c = matrix[0].size();
+        int top = 0, right = c - 1, bottom = r - 1, left = 0;
+        char dir = 'e';
         vector<int> ans;
-        while ( ans.size() < m * n ) {
-            if ( c == 'e' ) {
-                for (int i = left; i <= right; ++i) {
+        while (top <= bottom && left <= right) {
+            if (dir == 'e') {
+                for(int i = left; i <= right; ++i) {
                     ans.push_back(matrix[top][i]);
                 }
                 ++top;
-                c = 's';
-            } else if ( c == 's' ) {
-                for (int i = top; i <= bottom; ++i) {
+                dir = 's';
+            } else if (dir == 's') {
+                for(int i = top; i <= bottom; ++i) {
                     ans.push_back(matrix[i][right]);
                 }
                 --right;
-                c = 'w';
-            } else if ( c == 'w' ) {
-                for (int i = right; i >= left; --i) {
+                dir = 'w';
+            } else if (dir == 'w') {
+                for(int i = right; i >= left; --i) {
                     ans.push_back(matrix[bottom][i]);
                 }
                 --bottom;
-                c = 'n';
-            } else if ( c == 'n' ) {
-                for (int i = bottom; i >= top; --i) {
+                dir = 'n';
+            } else if (dir == 'n') {
+                for(int i = bottom; i >= top; --i) {
                     ans.push_back(matrix[i][left]);
                 }
                 ++left;
-                c = 'e';
+                dir = 'e';
             }
         }
         return ans;
