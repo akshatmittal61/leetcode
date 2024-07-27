0415ce66fe255eb1ef4f1f4d79f9c17eea3bc7bd
Akshat Mittal <akshatmittal2506@gmail.com>
2024-07-27 16:25:39 +0530

    Time: 4 ms (79.4%), Space: 8.9 MB (42.26%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
+    bool isValid(char c) {
+        if (c >= 'a' && c <= 'z') return true;
+        else if (c >= 'A' && c <= 'Z') return true;
+        else if (c >= '0' && c <= '9') return true;
+        else return false;
+    }
+    bool isEqual(char a, char b) {
+        if (a >= 'A' && a <= 'Z') a = char(a - 'A' + 'a');
+        if (b >= 'A' && b <= 'Z') b = char(b - 'A' + 'a');
+        return a == b;
+    }
     bool isPalindrome(string s) {
-        string str = "";
-        for(auto c: s) {
-            if (c >= 'a' && c <= 'z') str += c;
-            else if (c >= 'A' && c <= 'Z') str += c - 'A' + 'a';
-            else if (c >= '0' && c <= '9') str += c;
-        }
-        int n = str.length();
-        for (int i = 0, j = n - 1; i < j; ++i, --j) {
-            if (str[i] != str[j]) return false;
+        int n = s.length();
+        for (int i = 0, j = n - 1; i < j;) {
+            if (!isValid(s[i])) {
+                ++i;
+                continue;
+            }
+            if (!isValid(s[j])) {
+                --j;
+                continue;
+            }
+            if(!isEqual(s[i], s[j])) return false;
+            ++i, --j;
         }
         return true;
     }
