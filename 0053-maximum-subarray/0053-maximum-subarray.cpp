d32c3dd70dc7d8c4492fc4ecb0122f0d171fbd3e
Akshat Mittal <akshatmittal2506@gmail.com>
2024-07-30 00:32:44 +0530

    Time: 80 ms (42.92%), Space: 68 MB (99.81%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,15 +1,13 @@
-class Solution
-{
-    public:
-        int maxSubArray(vector<int> &nums)
-        {
-            int ans = INT_MIN, curr = 0, n = nums.size();
-            for (int i = 0; i < n; ++i)
-            {
-                curr += nums[i];
-                ans = max(ans, curr);
-                if (curr < 0) curr = 0;
-            }
-            return ans;
+class Solution {
+public:
+    int maxSubArray(vector<int>& v) {
+        int ans = INT_MIN, curr = 0;
+        int n = v.size();
+        for( int i = 0; i < n; ++i ) {
+            curr += v[i];
+            ans = max(ans, curr);
+            curr = max(curr, 0);
         }
+        return ans;
+    }
 };
