8674aa55d16c0ed05c618064478d7b01a67b4f8a
Akshat Mittal <akshatmittal2506@gmail.com>
2024-07-30 01:44:56 +0530

    Time: 23 ms (75.31%), Space: 14.3 MB (6.79%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
-    int minimumLength(string s) {
+int minimumLength(string s) {
         int n = s.length();
-        int i = 0, j = n - 1;
-        while ( i < j && s[i] == s[j] ) {
-            char c = s[i];
-            while ( i <= j && s[i] == c ) ++i;
-            while ( i <= j && s[j] == c ) --j;
+        int l = 0, r = n - 1;
+        while(l < r) {
+            if (s[l] == s[r]) {
+                int i = l, j = r;
+                while(i < n - 1 && s[i] == s[i + 1])
+                    ++i;
+                while(j > 0 && s[j] == s[j - 1])
+                    --j;
+                l = i + 1, r = j - 1;
+            } else {
+                return max(r - l + 1, 0);
+            }
         }
-        return j - i + 1;
+        return max(r - l + 1, 0);
     }
 };
