ac3484ceab2ac72f3545cbcffbdf4dac0518a9a5
Akshat Mittal <akshatmittal2506@gmail.com>
2024-07-31 17:23:26 +0530

    Time: 16 ms (7.28%), Space: 18.8 MB (29.53%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,18 +1,17 @@
-class Solution
-{
-    public:
-        vector<int> majorityElement(vector<int> &nums)
-        {
-            int n = nums.size();
-            int k = floor(n / 3);
-            vector<int> ans;
-            sort(nums.begin(), nums.end());
-            for (int i = 0; i < n; ++i)
-            {
-                int c = 0;
-                while (i < n - 1 && nums[i] == nums[i + 1]) ++c, ++i;
-                if (c >= k) ans.push_back(nums[i]);
-            }
-            return ans;
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        map<int, int> m;
+        vector<int> ans;
+        int n = nums.size();
+        for(auto x: nums) {
+            m[x]++;
         }
+        for(auto it: m) {
+            if(it.second > ceil(n / 3 )) {
+                ans.push_back(it.first);
+            }
+        }
+        return ans;
+    }
 };
