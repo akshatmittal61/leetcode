bcbdb5699b6257a658e5c8ceae9e64357fb1209f
Akshat Mittal <akshatmittal2506@gmail.com>
2024-08-03 21:29:26 +0530

    Time: 0 ms (100%), Space: 11.6 MB (24.11%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,31 +1,21 @@
 class Solution {
-public:
-    int peak(vector<int>nums, int beg, int end){
-        int n=nums.size();
-        // int mid=end+(beg-end)/2;
-        int mid=(beg+end)/2;
-        while(beg<=end)
-        {
-            mid=(beg+end)/2;
-            cout<<mid<<' ';
-            if( (mid==0 || nums[mid-1]<nums[mid]) && (mid==n-1 || nums[mid+1]
-            <nums[mid]) )
-                return mid;
-            else if(nums[mid+1]>nums[mid])return peak(nums, mid+1, end);
-            else return peak(nums, beg, mid-1);
+    int peak(vector<int>& nums, int beg, int end) {
+        int n = nums.size();
+        while(beg <= end) {
+            int mid = (beg + end) / 2;
+            if (
+                (mid == 0 || nums[mid - 1] < nums[mid]) &&
+                (mid == n - 1 || nums[mid + 1] < nums[mid])
+            ) return mid;
+            else if (nums[mid + 1] > nums[mid])
+                return peak(nums, mid + 1, end);
+            else return peak(nums, beg, mid - 1);
         }
         return -1;
     }
-    int findPeakElement(vector<int>& arr) {
-        // cout<<endl;
-        // return peak(nums, 0, nums.size()-1);
-        int n=arr.size();
-    bool flag=false;
-    for(int i=0;i<n-1;++i){
-        if(arr[i]>arr[i+1]){
-            return i;
-        }
-    }
-    return n-1;
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        return peak(nums, 0 , n - 1);
     }
 };
