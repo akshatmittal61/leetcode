e3834b8311573875d817ef8e28f4b2968e8ccf0a
Akshat Mittal <akshatmittal2506@gmail.com>
2024-08-06 00:34:03 +0530

    Time: 7 ms (97.17%), Space: 10.3 MB (69.54%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        vector<int>v(26, 0);
-        for(auto c: magazine) {
-            v[c - 'a']++;
-        }
-        for(auto c: ransomNote) {
-            v[c - 'a']--;
-        }
-        for(auto x: v) {
-            if(x < 0) return false;
+        vector<int> v(26, 0);
+        for (auto c: magazine) v[c - 'a']++;
+        for (auto c: ransomNote) v[c - 'a']--;
+        for (auto x: v) {
+            if (x < 0)
+                return false;
         }
         return true;
     }
