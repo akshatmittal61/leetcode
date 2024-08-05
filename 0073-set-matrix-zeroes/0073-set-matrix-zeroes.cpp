970aa69f7b88018c92230b562a219509c09abb50
Akshat Mittal <akshatmittal2506@gmail.com>
2024-08-06 02:43:04 +0530

    Time: 4 ms (95.55%), Space: 16.8 MB (48.06%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,29 +1,41 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        set<int> r, c;
+        bool r = false, c = false;
         int n = matrix.size(), m = matrix[0].size();
+        for (int i = 0; i < n; ++i) {
+            if (matrix[i][0] == 0) {
+                c = true;
+                break;
+            }
+        }
+        for (int i = 0; i < m; ++i) {
+            if (matrix[0][i] == 0) {
+                r = true;
+                break;
+            }
+        }
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
                 if (matrix[i][j] == 0) {
-                    r.insert(i);
-                    c.insert(j);
+                    matrix[i][0] = 0, matrix[0][j] = 0;
                 }
             }
         }
-        for(int x: r) cout<<x<<' ';
-        cout<<endl;
-        for(int x: c) cout<<x<<' ';
-        cout<<endl;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (
-                    (r.find(i) != r.end()) ||
-                    (c.find(j) != c.end())
-                ) {
+        for (int i = 1; i < n; ++i) {
+            for (int j = 1; j < m; ++j) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
+        }
+        if (r) {
+            for (int i = 0; i < m; ++i)
+                matrix[0][i] = 0;
+        }
+        if (c) {
+            for (int i = 0; i < n; ++i)
+                matrix[i][0] = 0;
         }
     }
 };
