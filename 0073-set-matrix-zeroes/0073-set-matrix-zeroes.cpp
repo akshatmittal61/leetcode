d412053762f607e5ef416ef82ab4f1e956fd4a1b
Akshat Mittal <akshatmittal2506@gmail.com>
2024-08-06 02:12:02 +0530

    Time: 7 ms (83.24%), Space: 17.1 MB (7%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        set<int>r, c;
-        int m = matrix.size(), n = matrix[0].size();
-        for(int i = 0; i < m; ++i) {
-            for(int j = 0; j < n; ++j) {
-                if(matrix[i][j] == 0) {
+        set<int> r, c;
+        int n = matrix.size(), m = matrix[0].size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (matrix[i][j] == 0) {
                     r.insert(i);
                     c.insert(j);
                 }
             }
         }
-        for(int i = 0; i < m; ++i) {
-            for(int j = 0; j < n; ++j) {
-                if(r.find(i) != r.end() || c.find(j) != c.end()) {
+        for(int x: r) cout<<x<<' ';
+        cout<<endl;
+        for(int x: c) cout<<x<<' ';
+        cout<<endl;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (
+                    (r.find(i) != r.end()) ||
+                    (c.find(j) != c.end())
+                ) {
                     matrix[i][j] = 0;
                 }
             }
