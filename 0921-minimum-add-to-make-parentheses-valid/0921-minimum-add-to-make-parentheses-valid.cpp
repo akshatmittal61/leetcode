574a1a1d4a5c37e7b2a51b121f0743f197a92fd1
Akshat Mittal <akshatmittal2506@gmail.com>
2024-08-25 00:20:26 +0530

    Time: 0 ms (100%), Space: 7.7 MB (63.59%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> st;
+        int ans = 0;
+        for(char c: s) {
+            if (c == '(') {
+                st.push(c);
+            } else {
+                if (st.empty()) ++ans;
+                else st.pop();
+            }
+        }
+        return st.size() + ans;
+    }
+};
